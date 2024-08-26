8adfc70b961da57e7f856f3ff1f4b2987ac11018
prashant kumar singh <prashant.singh2012p@gmail.com>
2024-08-26 23:47:46 +0530

    flask code done

--- house/logger/__init__.py
+++ house/logger/__init__.py
@@ -1,6 +1,7 @@
 import logging 
 from datetime import datetime 
 import os 
+import pandas as pd
 
 LOG_DIR= "housing_logs"
 
@@ -20,6 +21,17 @@ LOG_FILE_PATH =os.path.join(LOG_DIR,LOG_FILE_NAME)
 logging.basicConfig(
 filename=LOG_FILE_PATH,
 filemode="w",
-format='[%(asctime)s] %(name)s -%(levelname)s - %(message)s',
-level=logging.INFO                  
+format='[%(asctime)s]^;%(levelname)s^;%(lineno)d^;%(filename)s^;%(funcName)s()^;%(message)s',
+level=logging.INFO
 )
+
+def get_log_dataframe(file_path):
+    data=[]
+    with open(file_path) as log_file:
+        for line in log_file.readlines():
+            data.append(line.split("^;"))
+        log_df=pd.DataFrame(data)
+        columns=["Time Stamp","Log level","line number","file name","Function name","message"]
+        log_df.columns=columns
+        log_df["log_message"] = log_df['Time Stamp'].astype(str)+":&"+log_df["message"]
+        return log_df[["_log_message"]]
